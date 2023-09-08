f944928f7d3fe7d63f3853d0044d8c502b744a5d
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-08 16:26:11 -0500

    chore(ui): added angular material needed dependencies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    BrowserAnimationsModule, MatButtonModule, MatToolbarModule, MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
